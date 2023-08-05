2b0f7359004011ac2c22adfa8d2d42660b273642
abiolafasanya <hezekiahfasanya@gmail.com>
2023-08-05 23:36:07 +0100

    feat: details page

--- js/script.js
+++ js/script.js
@@ -31,37 +31,36 @@ const toggleBackground = () => {
 applyTheme();
 themeSwitch.addEventListener("click", toggleBackground);
 
-function fetchData() {
-  const url = "https://abiolafasanya.github.io/country-api/data.json"
-  return fetch(url || 'data.json')
-    .then((response) => response.json())
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-      return [];
-    });
+async function fetchData() {
+  const url = "https://abiolafasanya.github.io/country-api/data.json";
+  try {
+    const response = await fetch(url || "data.json");
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
 }
 
 const filterOption = document.querySelector("#filter");
 const filterPanel = document.querySelector("#filter-panel");
-
 const currentSelection = document.querySelector(".main-selection span");
 filterPanel.addEventListener("click", openSelection);
 
 document.addEventListener("click", (event) => {
   const isClickInsidePanel = filterPanel.contains(event.target);
   if (!isClickInsidePanel) {
-    // Clicked outside the filterPanel, so close the panel
     filterOption.innerHTML = "";
-    filterPanel.classList.add("hidden");
+    filterPanel.classList.add("close");
   }
 });
 
-filterPanel.classList.add("hidden");
+filterPanel.classList.add("close");
 function openSelection() {
-  const isPanelHidden = filterPanel.classList.contains("hidden");
+  const isPanelHidden = filterPanel.classList.contains("close");
   if (isPanelHidden) {
     // Open the panel and fetch data
-    filterPanel.classList.remove("hidden");
+    filterPanel.classList.remove("close");
     getUniqueRegions().then((data) => {
       const selectElement = document.createElement("div");
       data.forEach((region) => {
@@ -72,65 +71,61 @@ function openSelection() {
         optionElement.addEventListener("click", () => {
           const selectedOptionText = optionElement.textContent;
           currentSelection.textContent = selectedOptionText;
-          filterOption.classList.add("hidden");
-
-          // Call the renderCountryCards function when the selection is made
+          filterOption.classList.add("close");
           renderCountryCards();
         });
       });
-      // Before appending the selectElement, clear the previous options
       filterOption.innerHTML = "";
       filterOption.appendChild(selectElement);
     });
   } else {
     filterOption.innerHTML = "";
-    filterPanel.classList.add("hidden");
+    filterPanel.classList.add("close");
   }
-  // Promise.resolve().then(() => {
-  //   console.log("state");
-  // });
 }
 
 const countriesContainer = document.querySelector("#countries-container");
 let countryData;
+
 fetchData().then((data) => {
   countryData = data;
   renderCountryCards();
 });
 
 function renderCountryCards(filter = "") {
-  let countriesToShow;
+  // let countriesToShow;
   if (filter !== "") {
-    countriesToShow = countryData.filter((country) =>
+    const data = countryData.filter((country) =>
       country.name.toLowerCase().includes(filter)
     );
+    getCountryListing(data);
   } else {
-    // Get the currently selected region
     const selectedRegion = currentSelection.textContent.trim();
-    // Convert the selected region to lowercase for comparison
     const selectedRegionLowerCase = selectedRegion.toLowerCase();
 
-    countriesToShow = countryData.filter(
+    const data = countryData.filter(
       (country) => country.region.toLowerCase() === selectedRegionLowerCase
     );
+    getCountryListing(data);
   }
-    console.log(countryData);
-    console.log(countriesToShow);
-    console.log({res: currentSelection.textContent});
-    
+}
+
+function getCountryListing(countriesToShow) {
+  // Clear the previous content in the countriesContainer
+  countriesContainer.innerHTML = "";
+
   countriesToShow.forEach((option) => {
     const countryCard = document.createElement("div");
+    countryCard.classList.add("country-card");
+
     const countryImage = document.createElement("img");
     const countryName = document.createElement("h3");
     const countryPopulation = document.createElement("h5");
     const countryRegion = document.createElement("h5");
     const countryCapital = document.createElement("h5");
     const countryContent = document.createElement("article");
-
     countryCard.setAttribute("data-region", option.region);
-    countryCard.classList.add("country-card");
 
-    // if (currentSelection.innerHTML.match(option.region)) {
     countryImage.src = option.flags.svg;
     countryName.textContent = option.name;
     countryCapital.innerHTML = `<b>Capital:</b>  <span>${option.capital}</span>`;
@@ -142,11 +137,13 @@ function renderCountryCards(filter = "") {
     countryContent.appendChild(countryRegion);
     countryContent.appendChild(countryCapital);
 
-    // Append elements to countryCard in the desired order
     countryCard.appendChild(countryImage);
     countryCard.appendChild(countryContent);
-    countriesContainer.innerHTML = "";
+
+    // Append each country card to the countriesContainer
     countriesContainer.appendChild(countryCard);
+
+    countryCard.addEventListener("click", (e) => renderDetailsPage(option));
   });
 }
 
@@ -163,3 +160,64 @@ searchInput.addEventListener("input", () => {
   const searchText = searchInput.value.trim().toLowerCase();
   renderCountryCards(searchText);
 });
+
+const item1 = document.querySelector("#section1");
+const item2 = document.querySelector("#countries-container");
+
+document.querySelector(".back-button").addEventListener("click", (e) => {
+  e.preventDefault();
+  item1.style.display = "flex";
+  item2.style.display = "flex";
+});
+
+function renderDetailsPage(option) {
+  const countryDetail = document.querySelector("#countryDetail");
+  countryDetail.innerHTML = "";
+  item1.style.display = "none";
+  item2.style.display = "none";
+
+  const countryCard = `
+  <div class="country-data" data-region="${option.region}">
+    <div class="img-card">
+      <img src="${option.flags.svg}" alt="${option.name}" />
+    </div>
+    <article class="country-desc">
+      <h3>${option.name}</h3>
+      <div class="country-bio">
+        <div>
+          <p><b>Native Name:</b> <span>${option.nativeName}</span></p>
+          <p><b>Population:</b> <span>${option.population}</span></p>
+          <p><b>Region:</b> <span>${option.region}</span></p>
+          <p><b>Sub Region:</b> <span>${option.subregion}</span></p>
+          <p><b>Capital:</b> <span>${option.capital}</span></p>
+        </div>
+        <div>
+          <p><b>Top Level Domain:</b> <span>${option.topLevelDomain.join(
+            ", "
+          )}</span></p>
+          <p><b>Currencies:</b> <span>${option.languages
+            .map((lang) => lang.name)
+            .join(", ")}</span></p>
+          <p><b>Language:</b> <span>${option.currencies
+            .map((currency) => currency.code)
+            .join(", ")}</span></p>
+        </div>
+      </div>
+      <div class="border-group">
+      <h5>Border Countries</h5>
+      <div class="border">
+      ${
+        option?.borders
+          ? option?.borders.map(
+              (border) => `<div class="border-item">${border}</div>`
+            )
+          : ""
+      }
+      </div>
+      </div>
+    </article>
+  </div>
+`;
+
+  countryDetail.innerHTML += countryCard;
+}
